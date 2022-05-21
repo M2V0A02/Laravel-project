52df06bd1bf8d478b8271f7f8c385a7c70d9e47f
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-05-21 18:53:24 +0500

    Добавилена битва между двумя юнитами

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,9 @@
     <section class="battle_section">
         <div class="battle_panel">
             <div class="control_battle">
-                <input type="text" id="id_cell" value="p-1">
+                <input type="text" id="id_cell" value="peasant-1">
                 <div class="container_control_battle">
-                    <button class="control_battle_button_select_unit"> Выбрать юнита</button>
+                    <button onclick="showDate()" class="control_battle_button_select_unit"> Выбрать юнита</button>
                     <button onclick="moveTop()" class="control_battle_button_top">Вверх </button>
                     <button onclick="moveLeft()" class="control_battle_button_left">Влево</button>
                     <button onclick="moveRight()" class="control_battle_button_right">Вправо </button>
@@ -23,22 +23,30 @@
                 </div>
             </div>
             <div class="info_battle_panel">
-                <p> Название: <p>
-                <p> Количество моделек: </p>
-                <p> Здоровье: </p>
-                <p> Броня: </p>
-                <p> Урон: </p>
-                <p> Защита: </p>
-                <p> Атака: </p>
+                <p class="info_battle_panel_name"> Название: <p>
+                <p class="info_battle_panel_amount"> Количество моделек: </p>
+                <p class="info_battle_panel_health"> Здоровье: </p>
+                <p class="info_battle_panel_armor"> Броня: </p>
+                <p class="info_battle_panel_damage"> Урон: </p>
+                <p class="info_battle_panel_defense"> Защита: </p>
+                <p class="info_battle_panel_attack"> Атака: </p>
             </div>
         </div>
         <div>
             <table class="battle_field">
                 <tbody>
                     <tr class="battle_field_line">
+                        <td  class="battle_field_cell"></td>
                         <td class="battle_field_cell"></td>
                         <td class="battle_field_cell"></td>
                         <td class="battle_field_cell"></td>
+                        <td class="battle_field_cell"></td>
+                        <td class="battle_field_cell"></td>
+                    </tr>
+                    <tr class="battle_field_line">
+                        <td class="battle_field_cell"></td>
+                        <td class="battle_field_cell"></td>
+                        <td id="robber-1" class="battle_field_cell">R</td>
                         <td class="battle_field_cell"></td>
                         <td class="battle_field_cell"></td>
                         <td class="battle_field_cell"></td>
@@ -68,15 +76,7 @@
                         <td class="battle_field_cell"></td>
                     </tr>
                     <tr class="battle_field_line">
-                        <td class="battle_field_cell"></td>
-                        <td class="battle_field_cell"></td>
-                        <td class="battle_field_cell"></td>
-                        <td class="battle_field_cell"></td>
-                        <td class="battle_field_cell"></td>
-                        <td class="battle_field_cell"></td>
-                    </tr>
-                    <tr class="battle_field_line">
-                        <td id="p-1" class="battle_field_cell">P</td>
+                        <td id="peasant-1" class="battle_field_cell">P</td>
                         <td id="p-2" class="battle_field_cell">P</td>
                         <td class="battle_field_cell"></td>
                         <td class="battle_field_cell"></td>
@@ -89,9 +89,45 @@
     </section>
 </body>
 <script>
+    class Unit {
+        constructor(name, armor, health, amountOfUnits,
+                    attack, defense, damage){
+            this.name = name;
+            this.armor = armor;
+            this.health = health;
+            this.amountOfUnits = amountOfUnits;
+            this.attack = attack;
+            this.defense = defense;
+            this.damage = damage;
+        }
+
+    }
+
+    function showDate(){
+        let peasant = JSON.parse(localStorage.getItem('peasant-1'));
+        document.querySelector(".info_battle_panel_name").innerText = "Название: " + peasant.name;
+        document.querySelector(".info_battle_panel_armor").innerText = "Броня: " + peasant.armor;
+        document.querySelector(".info_battle_panel_health").innerText = "Здоровье: " + peasant.health;
+        document.querySelector(".info_battle_panel_amount").innerText = "Количество солдат: " + peasant.amountOfUnits;
+        document.querySelector(".info_battle_panel_attack").innerText = "Атака: " + peasant.attack;
+        document.querySelector(".info_battle_panel_defense").innerText = "Защита: " + peasant.defense;
+        document.querySelector(".info_battle_panel_damage").innerText = "Урон: " + peasant.damage;
+    }
+
     function moveTop(){
         elements = document.querySelector('#' + id_cell.value).parentNode.childNodes;
-        element = document.querySelector('#' + id_cell.value)
+        element = document.querySelector('#' + id_cell.value);
+        // Проверка есть ли что-нибудь на пути
+        for (var i = 0; i < elements.length; i++) {
+            if (elements[i].id == id_cell.value) {
+                positionElement = i;
+                if (element.parentNode.previousSibling.previousSibling.childNodes[positionElement].id != '') {
+                    battle(id_cell.value, element.parentNode.previousSibling.previousSibling.childNodes[positionElement].id);
+                    return 0;
+                }
+                break;
+            }
+        }
         for (var i = 0; i < elements.length; i++) {
             if (elements[i].id == id_cell.value) {
                 elements[i].id = ''
@@ -108,6 +144,18 @@
     function moveDown(){
         elements = document.querySelector('#' + id_cell.value).parentNode.childNodes;
         element = document.querySelector('#' + id_cell.value)
+
+        for (var i = 0; i < elements.length; i++) {
+            if (elements[i].id == id_cell.value) {
+                positionElement = i;
+                if (element.parentNode.nextSibling.nextSibling.childNodes[positionElement].id != '') {
+                    battle(id_cell.value, element.parentNode.nextSibling.nextSibling.childNodes[positionElement].id);
+                    return 0;
+                }
+                break;
+            }
+        }
+
         for (var i = 0; i < elements.length; i++) {
             if (elements[i].id == id_cell.value) {
                 elements[i].id = ''
@@ -123,6 +171,10 @@
 
     function moveRight(){
         el = document.querySelector("#" + id_cell.value);
+        if (el.nextSibling.nextSibling.id != '') {
+            battle(id_cell.value, el.nextSibling.nextSibling.id);
+            return 0;
+        }
         el.nextSibling.nextSibling.innerHTML = el.innerHTML;
         el.innerHTML = "";
         el.nextSibling.nextSibling.id = el.id;
@@ -131,12 +183,51 @@
 
     function moveLeft(){
         el = document.querySelector("#" + id_cell.value);
+        if (el.previousSibling.previousSibling.id != '') {
+            battle(id_cell.value, el.previousSibling.previousSibling.id);
+            return 0;
+        }
         el.previousSibling.previousSibling.innerHTML = el.innerHTML;
         el.innerHTML = "";
         el.previousSibling.previousSibling.id = el.id;
         el.id = "";
     }
-    battle_field = document.querySelector('.battle_field');
-    
+
+    function hitChance(attack, defense){
+        return 30 - defense + attack;
+    }
+
+    function battle(idUnit1, idUnit2){
+        unit1 = JSON.parse(localStorage.getItem(idUnit1));
+        unit2 = JSON.parse(localStorage.getItem(idUnit2));
+        while ((unit1.amountOfUnits > 0) && (unit2.amountOfUnits > 0)) {
+            damageUnit1 = unit1.amountOfUnits / 100 * Math.random(1, 100)  *
+                          hitChance(unit1.attack, unit2.defense) * unit1.damage;
+            console.log(damageUnit1)
+            damageUnit2 = unit2.amountOfUnits / 100 * Math.random(1, 100)  *
+                          hitChance(unit2.attack, unit1.defense) * unit2.damage;
+            console.log(damageUnit2)
+            unit1.amountOfUnits -= Math.round(damageUnit2 / unit1.health);
+            unit2.amountOfUnits -= Math.round(damageUnit1 / unit2.health);
+        }
+        console.log(unit1.amountOfUnits)
+        console.log(unit2.amountOfUnits)
+        if (unit1.amountOfUnits <= 0)
+            destroyUnit(idUnit1)
+        if (unit2.amountOfUnits <= 0)
+            destroyUnit(idUnit2) 
+    }
+
+    function destroyUnit(idUnit){
+        localStorage.removeItem(idUnit);
+        document.getElementById(idUnit).innerText = '';
+        document.getElementById(idUnit).id = '';
+    }
+
+    localStorage.setItem('peasant-1', JSON.stringify(new Unit("Крестьянин", 10, 50, 90, 12, 20, 20)));
+    localStorage.setItem('robber-1', JSON.stringify(new Unit("Грабитель", 20, 60, 90, 30, 26, 20)));
+    var battle_field = document.querySelector('.battle_field');
+
+
 </script>
 </html>
